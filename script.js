fe8563ee663f59a49cbda96ffb28142f4d5104af
Cristiano Sales <cristianosalesneves@gmail.com>
2021-12-03 08:42:29 -0300

    Requisito 3, ok

---
+++ script.js
@@ -0,0 +1,33 @@
+const header = document.querySelector('header');
+const img = document.querySelector('trybewarts-header-logo');
+const form = document.querySelector('trybewarts-login');
+const login = document.getElementById('input-email');
+const senha = document.getElementById('input-senha');
+const button = document.getElementById('btn1');
+
+function checkLogin () {
+    if (login.value === 'tryber@teste.com' && senha.value === '123456') {
+        alert('Olá, Tryber!');
+    } else {
+        alert('Email ou senha inválidos.');
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+function listeners() {
+    button.addEventListener('click', checkLogin);
+}
+    window.onload = function init() {
+        listeners();
+    }
+
+  
