181158f5dfe87a92108e6259f2c5b54b8e58786c
luis fernando cutilak <luiscutilak@gmail.com>
2021-12-06 01:21:00 -0300

    Requisito 18 e 19, ok

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const login = document.getElementById('input-email-cabecalho');
 const senha = document.getElementById('input-senha');
 const button = document.getElementById('btn1');
-const agreement = document.getElementById 
+const agreement = document.getElementById('agreement');
+const submitButton = document.getElementById('submit-btn');
+submitButton.disabled = true;
 
 function checkLogin() {
   if (login.value === 'tryber@teste.com' && senha.value === '123456') {
@@ -10,9 +12,16 @@ function checkLogin() {
     alert('Email ou senha inválidos.');
   }
 }
-
+function checkedBox() {
+  if (agreement.checked === true) {
+    submitButton.disabled = false;
+  } else {
+    submitButton.disabled = true;
+  }
+}
 function listeners() {
   button.addEventListener('click', checkLogin);
+  agreement.addEventListener('click', checkedBox);
 }
 window.onload = function init() {
   listeners();
